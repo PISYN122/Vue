a3003295ff82736edc77aedf7d52abfa1c349dc2
Ходанич Олександр Васильович <sasha.chdan@gmail.com>
2024-11-10 01:00:32 +0200

    22222

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router'; // підключаємо роутер
-
-createApp(App)
-  .use(router) // використовуємо роутер
-  .mount('#app');
